ea1bc76190fdc6e8e4d947a175fd791eb037e328
Yeeun Jung <rjyeeun@gmail.com>
2022-12-17 16:19:40 -0500

    commit tree 748544546e7451950c3109364c0a51a87d60af2f

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(books => { 
+   (renderBooks(books))
+  })
+}
+
   // To pass the tests, don't forget to return your fetch!
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +22,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
